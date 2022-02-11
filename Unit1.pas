66401b485c2e7114290cb6389c35acee92f4021f
toufikben <66898475+toufikben@users.noreply.github.com>
2022-02-11 15:02:56 +0100

    update
    added windows media player
    fix some bugs

--- Unit1.pas
+++ Unit1.pas
@@ -8,7 +8,7 @@ uses
   Vcl.NumberBox, Vcl.StdCtrls, Vcl.WinXCtrls, Vcl.ComCtrls, Data.DB, Vcl.Grids,
   Vcl.DBGrids, Vcl.ExtCtrls, Vcl.MPlayer, Vcl.Buttons,system.UITypes,
   Vcl.Imaging.pngimage,Winapi.ShellAPI, Vcl.WinXPickers,DateUtils,System.IOUtils,Vcl.Themes,Vcl.Styles,
-  system.Types;
+  system.Types, Vcl.OleCtrls, WMPLib_TLB;
 
 type
   Tmain = class(TForm)
@@ -22,7 +22,6 @@ type
     OpenDialog1: TOpenDialog;
     Panel1: TPanel;
     Label1: TLabel;
-    Button1: TButton;
     MediaPlayer1: TMediaPlayer;
     TrackBar1: TTrackBar;
     Timer1: TTimer;
@@ -72,6 +71,14 @@ type
     ProgressBar1: TProgressBar;
     OpenDialog2: TOpenDialog;
     BitBtn5: TBitBtn;
+    btn6: TBitBtn;
+    tmr1: TTimer;
+    WindowsMediaPlayer1: TWindowsMediaPlayer;
+    BitBtn6: TBitBtn;
+    BitBtn7: TBitBtn;
+    BitBtn8: TBitBtn;
+    BitBtn9: TBitBtn;
+    BitBtn10: TBitBtn;
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure Button1Click(Sender: TObject);
     procedure Timer1Timer(Sender: TObject);
@@ -88,12 +95,15 @@ type
     procedure Timer2Timer(Sender: TObject);
     procedure BitBtn3Click(Sender: TObject);
     procedure btn1Click(Sender: TObject);
-    procedure MediaPlayer1Click(Sender: TObject; Button: TMPBtnType;
-      var DoDefault: Boolean);
     procedure FormCreate(Sender: TObject);
     procedure BitBtn2Click(Sender: TObject);
     procedure BitBtn4Click(Sender: TObject);
     procedure BitBtn5Click(Sender: TObject);
+    procedure btn6Click(Sender: TObject);
+    procedure tmr1Timer(Sender: TObject);
+    procedure BitBtn7Click(Sender: TObject);
+    procedure BitBtn8Click(Sender: TObject);
+    procedure BitBtn9Click(Sender: TObject);
   private
   procedure StylesListRefresh;
     { Private declarations }
@@ -204,6 +214,24 @@ begin
 ShellExecute(Handle,'open', PChar('"' +(ExtractFilePath (application.ExeName))+'backup'+'"'),nil,nil, SW_SHOWNORMAL);
 end;
 
+procedure Tmain.BitBtn7Click(Sender: TObject);
+begin
+ MediaPlayer1.Stop;
+ WindowsMediaPlayer1.controls.stop;
+end;
+
+procedure Tmain.BitBtn8Click(Sender: TObject);
+begin
+  dm.FDQuery1.Next;
+    Button1Click(Sender);
+  end;
+
+procedure Tmain.BitBtn9Click(Sender: TObject);
+begin
+ dm.FDQuery1.Prior;
+    Button1Click(Sender);
+end;
+
 procedure Tmain.LoadImage();
 begin
   if ( dm.FDQuery1.IsEmpty = true )  then
@@ -217,14 +245,6 @@ begin
 //   tm2.Time := TStringField( dm.FDQuery1.FieldByName('end_audio') ).asdatetime;
   end;
 
-
-procedure Tmain.MediaPlayer1Click(Sender: TObject; Button: TMPBtnType;
-  var DoDefault: Boolean);
-begin
-
-//if MediaPlayer1.FileName='' then
-//   ShowMessage('·„ Ì „ «Œ Ì«— „·› ’Ê Ì «Ê ·«ÌÊÃœ „·› ’Ê Ì «’·«');    MediaPlayer1.Close ;
-end;
 
 procedure Tmain.SearchBox1Change(Sender: TObject);
 begin
@@ -260,13 +280,7 @@ end;
 
 procedure Tmain.BitBtn1Click(Sender: TObject);
 begin
- Timer1.Enabled := False;
- edt1.text  := '';
- memo1.text  := '';
- cb1.ItemIndex :=-1;
- TM1.Time := 0;
- TM2.Time := 0;
-
+ ShellExecute(Handle, 'OPEN', pchar('explorer.exe'), pchar('/select, "' +(ExtractFilePath (application.ExeName))+ dm.FDQuery1.FieldByName('Fatwa_audio_loction').Asstring+ '"'),   nil,  SW_NORMAL);
 end;
 
 procedure Tmain.BitBtn2Click(Sender: TObject);
@@ -350,7 +364,19 @@ ShowMessage('«·Õﬁ· „ÊÃÊœ »«·›⁄·. «·„—ÃÊ «⁄«œ… «·„Õ«Ê·…');
 end
 else
 begin
-  AppDir := 'Fatwa_audio_loction'+'\';
+//  AppDir := 'Fatwa_audio_loction'+'\';
+    if cb1.ItemIndex = 0 then
+  begin
+  AppDir := 'Fatwa_audio_loction'+'\Al_Albani'+'\';
+  end;
+  if cb1.ItemIndex = 1 then
+  begin
+    AppDir := 'Fatwa_audio_loction'+'\ibn_uthaymeen'+'\';
+  end;
+  if cb1.ItemIndex = 2 then
+  begin
+  AppDir := 'Fatwa_audio_loction'+'\ibn_baaz'+'\';
+  end;
   path := ExtractFilePath (application.ExeName);
   if OpenDialog1.Execute then
   if OpenDialog1.FileName <>'' then
@@ -403,7 +429,19 @@ end
 else
 begin
 //  AppDir := ExtractFileDir(Application.ExeName)+'\Fatwa_audio_loction\';
-  AppDir := 'Fatwa_audio_loction'+'\';
+  if cb1.ItemIndex = 0 then
+  begin
+  AppDir := 'Fatwa_audio_loction'+'\Al_Albani'+'\';
+  end;
+  if cb1.ItemIndex = 1 then
+  begin
+    AppDir := 'Fatwa_audio_loction'+'\ibn_uthaymeen'+'\';
+  end;
+  if cb1.ItemIndex = 2 then
+  begin
+  AppDir := 'Fatwa_audio_loction'+'\ibn_baaz'+'\';
+  end;
+
   path := ExtractFilePath (application.ExeName);
   if OpenDialog1.Execute then
   if OpenDialog1.FileName <>'' then
@@ -479,6 +517,17 @@ begin
    end;
 end;
 
+procedure Tmain.btn6Click(Sender: TObject);
+begin
+ Timer1.Enabled := False;
+ edt1.text  := '';
+ memo1.text  := '';
+ cb1.ItemIndex :=-1;
+ TM1.Time := 0;
+ TM2.Time := 0;
+ SearchBox2.Text :='';
+end;
+
 procedure Tmain.btn7Click(Sender: TObject);
 begin
 ShellExecute(Handle, 'open','https://www.facebook.com/Yamada.Fakir1',nil,nil, SW_SHOWNORMAL) ;
@@ -493,15 +542,20 @@ procedure Tmain.Button1Click(Sender: TObject);
 var
   path : string;
  begin
+
+//if MediaPlayer1.FileName='' then
+//   ShowMessage('·„ Ì „ «Œ Ì«— „·› ’Ê Ì «Ê ·«ÌÊÃœ „·› ’Ê Ì «’·«');    MediaPlayer1.Close
     path := ExtractFilePath (application.ExeName);
   if (dm.FDQuery1.FieldByName('Fatwa_audio_loction').Text.IsEmpty  =true ) or  not FileExists(path+ dm.FDQuery1.FieldByName('Fatwa_audio_loction').asString) then
   begin
 
  ShowMessage('Œÿ√ ›Ì  ‘€Ì· «·„·› «·’Ê Ì  Õﬁﬁ „‰ ÊÃÊœÂ »„”«—Â «·’ÕÌÕ')
-end else
-begin
+   end else   begin
  Timer1.Enabled:=False;
  Timer2.Enabled:=False;
+ tmr1.Enabled:=False;
+ if sw2.State =tsson  then
+  begin
  MediaPlayer1.FileName:=(path+ dm.FDQuery1.FieldByName('Fatwa_audio_loction').Asstring);
  MediaPlayer1.Open;
 
@@ -515,13 +569,21 @@ begin
  TrackBar1.Max:=MediaPlayer1.TrackLength[1];
  Timer1.Enabled:=True;
  MediaPlayer1.Play;
-
-//  SearchBox1.Text := MediaPlayer1.TrackLength[1].ToString;
+   end else
+   begin
+  if sw2.State =tssOff then
+  WindowsMediaPlayer1.URL :=(path+ dm.FDQuery1.FieldByName('Fatwa_audio_loction').Asstring);
+  WindowsMediaPlayer1.controls.play;
+  windowsMediaPlayer1.controls.currentPosition:= (TStringField( dm.FDQuery1.FieldByName('start_audio') ).AsInteger);
+  tmr1.Enabled:=True;
+//   edt1.text:=inttostr((TStringField( dm.FDQuery1.FieldByName('start_audio') ).AsInteger));
+////  SearchBox1.Text := MediaPlayer1.TrackLength[1].ToString;
 
  //Obtenir_la_duree(MediaPlayer1.TrackLength[1],heure,Minute,Seconde);la.Caption :=  heure.ToString+Minute.ToString+Seconde.ToString  ;
 // get_duration(MediaPlayer1.TrackLength[1],duration,':');la.Caption :=  duration  ;
    //     chaine_de_ficelle:=chaine_de_ficelle+utile.CRLF;
-end;
+   end;
+   end;
 end;
 
 procedure Tmain.FormActivate(Sender: TObject);
@@ -534,17 +596,31 @@ dm.FDQuery1.Active := True;
  begin
  CreateDir(ExtractFilePath (application.ExeName)+'Fatwa_audio_loction');
 end;
+
+ if not DirectoryExists(ExtractFilePath (application.ExeName)+'Fatwa_audio_loction'+'\Al_Albani') then
+ begin
+ CreateDir(ExtractFilePath (application.ExeName)+'Fatwa_audio_loction'+'\Al_Albani');
+end;
+
+ if not DirectoryExists(ExtractFilePath (application.ExeName)+'Fatwa_audio_loction'+'\ibn_uthaymeen') then
+ begin
+ CreateDir(ExtractFilePath (application.ExeName)+'Fatwa_audio_loction'+'\ibn_uthaymeen');
+end;
+
+ if not DirectoryExists(ExtractFilePath (application.ExeName)+'Fatwa_audio_loction'+'\ibn_baaz') then
+ begin
+ CreateDir(ExtractFilePath (application.ExeName)+'Fatwa_audio_loction'+'\ibn_baaz');
+end;
 end;
 
 procedure Tmain.FormClose(Sender: TObject; var Action: TCloseAction);var
 vExeFile: string;
   RS: TResourceStream;
 begin
+WindowsMediaPlayer1.controls.stop;
   if FileExists(ExtractFilePath (application.ExeName)+'sound\bhamdala.wav')   then
  begin  sndPlaySound('sound\bhamdala.wav',SND_SYNC );
-//Application.Terminate;
- form1.Close;
-// application.Destroy;
+
  end
   else
   begin
@@ -560,7 +636,9 @@ begin
     RS.Free;
   end;
    end;
-
+  form1.Close;
+  Application.Terminate;
+// application.Destroy;
 //MMSystem
  //  sndPlaySound('help\1.wav',SND_ASYNC );
    // SND_SYNC  =0 = Start playing, and wait for the sound to finish
@@ -586,13 +664,10 @@ heure := (MediaPlayer1.Position Div 3600000);
   Seconde := (MediaPlayer1.Position Mod 60000 )Div 1000 ;
 //
   lbl1.Caption := Format('%d:%2.2d:%2.2d ', [heure,Minute,Seconde]);
-
-//
 // lbl1.Caption := Format('%d:%2.2d:%2.2d', [(MediaPlayer1.Position div 3600000),(MediaPlayer1.Position div 60000),
 //                        (MediaPlayer1.Position mod 60000 div 1000)]);
 //if TrackBar1.Max <> 0 then
 //  TrackBar1.Position := MediaPlayer1.Position;
-
 TrackBar1.Position:=MediaPlayer1.Position;
 Label1.Caption:=FormatFloat('0#.##',(TrackBar1.Position/Trackbar1.Max)*100)+' %';
 if MediaPlayer1.Position=TrackBar1.Max then MediaPlayer1.Stop;
@@ -616,8 +691,16 @@ begin
   end;
 end;
 
+procedure Tmain.tmr1Timer(Sender: TObject);
+begin
+ if WindowsMediaPlayer1.controls.currentPosition >= (TStringField( dm.FDQuery1.FieldByName('end_audio') ).AsInteger) then
+ WindowsMediaPlayer1.controls.stop;
+end;
+
 procedure Tmain.TrackBar1Change(Sender: TObject);
 begin
+// if WindowsMediaPlayer1.controls.currentPosition >= ( TStringField( dm.FDQuery1.FieldByName('end_audio') ).AsInteger)* 1000 then
+// WindowsMediaPlayer1.controls.stop;
 
 end;
 //MediaPlayer1.Start := TrackBar1.Position;
